423baf350e746a38811d0636a918c736c160e062
ibbeuni <ibbeuniii@gmail.com>
2022-11-18 00:51:16 +0800

    Update HomePage.vue

--- front-end/src/views/HomePage.vue
+++ front-end/src/views/HomePage.vue
@@ -8,7 +8,7 @@
                 <div class="search-wrap">
                     <div class="logo-box">
                         <!-- TODO <a>首頁連結 -->
-                        <a href="/#/login">
+                        <a href="/#/member">
                             <img src="../assets/BookstoreLogo.png" alt="">
                         </a>
                     </div>
