7ffe25787d0d54ecd4467c617b6e79c1141f3c20
GavinWU1 <catmimi1212@gmail.com>
2022-11-21 22:49:47 +0800

    新增活動小圖片

--- front-end/src/views/EventDetail.vue
+++ front-end/src/views/EventDetail.vue
@@ -14,17 +14,59 @@
             </div>
         </div>
         <div class="bookimg">
-            <img src="../assets/img/event/event_info/event_101.jpeg" alt="">
+            <img src="../assets/event_img/event/event_101.jpeg" alt="">
         </div>
         <div class="bookname">
-            <ul>
-                <h4>其他書籍活動</h4>
-                <li><button><span>所有活動</span></button></li>
-                <li><button><span>所有活動</span></button></li>
-                <li><button><span>所有活動</span></button></li>
-                <li><button><span>所有活動</span></button></li>
-                <li><button><span>所有活動</span></button></li>
-            </ul>
+            <div>
+                <ul>
+                    <a href="" style="font-size: 25px">其他書籍活動</a><br><br>
+                    <li><button><span>所有活動</span></button></li>
+                    <li><button><span>收藏</span></button></li>
+                    <li><button><span>繪畫</span></button></li>
+                    <li><button><span>藝術</span></button></li>
+                    <li><button><span>攝影</span></button></li>
+                </ul>
+            </div>
+            <div>
+                <ul class="liflex" style="display:flex;">
+                    <li>
+                        <div>
+                            <a href="">
+                                <div>
+                                        <img src="../assets/event_img/event_minicover/event_minicover_101.jpeg" alt="">
+                                </div>
+                            </a>
+                        </div>
+                    </li>
+                    <li>
+                        <div>
+                            <a href="">
+                                <div>
+                                        <img src="../assets/event_img/event_minicover/event_minicover_101.jpeg" alt="">
+                                </div>
+                            </a>
+                        </div>
+                    </li>
+                    <li>
+                        <div>
+                            <a href="">
+                                <div>
+                                        <img src="../assets/event_img/event_minicover/event_minicover_101.jpeg" alt="">
+                                </div>
+                            </a>
+                        </div>
+                    </li>
+                    <li>
+                        <div>
+                            <a href="">
+                                <div>
+                                        <img src="../assets/event_img/event_minicover/event_minicover_101.jpeg" alt="">
+                                </div>
+                            </a>
+                        </div>
+                    </li>
+                </ul>
+            </div>
         </div>
 
 
@@ -56,15 +98,35 @@
     margin: 30px;
     /* text-align: center; */
 }
-
 .bookimg {
     text-align: center;
 }
 
 button {
-    border-radius: 10px
+    display: inline-block;
+    text-align: center;
+    vertical-align: middle;
+    padding: 0px 20px;
+    border: 0px solid #e9e8e8;
+    border-radius: 50px;
+    background: #e9e8e8;
+    font: normal normal bold 20px arial;
+    color: #272727;
+    text-decoration: none;
+    text-transform: uppercase
 }
-li{
+
+button:hover,
+button:focus {
+    background: #454545;
+    color: #cfcece;
+}
+
+li {
     display: inline;
+    margin-right: 10px;
+}
+.liflex li{
+    flex: 25%;
 }
 </style>
