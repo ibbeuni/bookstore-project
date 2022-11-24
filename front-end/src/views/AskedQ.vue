d3d4b23c5c1766797b90acb0606f4573b1479a95
GavinWU1 <catmimi1212@gmail.com>
2022-11-24 16:05:36 +0800

    彈一曲手風琴

--- front-end/src/views/AskedQ.vue
+++ front-end/src/views/AskedQ.vue
@@ -20,27 +20,42 @@
                 </ul>
             </div>
             <div class="card_1">
-                <div class="accordion-body">
-                    <div class="accordion">
-                        <h1>會員常見問題</h1>
-                        <hr>
-                        <div class="container">
-                            <div class="label">Ｑ : 忘記帳號密碼怎麼辦？</div>
-                            <div class="content">Ａ : 可以直接來信或來電與客服人員聯繫協助查詢，忘記密碼則可以點選忘記密碼，我們會發email到您的信箱內，買家可以自行收信更改密碼唷!
+                <div class="accordion" id="myAccordion">
+                    <h1>會員常見問題</h1>
+                    <div class="accordion-item">
+                        <div class="accordion-header" id="headingOne">
+                            <button type="button" class="accordion-button collapsed" data-bs-toggle="collapse"
+                                data-bs-target="#collapseOne">Ｑ : 忘記帳號密碼怎麼辦？</button>
+                        </div>
+                        <div id="collapseOne" class="accordion-collapse collapse" data-bs-parent="#myAccordion">
+                            <div class="card-body">
+                                <p>Ａ : 可以直接來信或來電與客服人員聯繫協助查詢，忘記密碼則可以點選忘記密碼，我們會發email到您的信箱內，買家可以自行收信更改密碼唷!</p>
                             </div>
                         </div>
-                        <hr>
-                        <div class="container">
-                            <div class="label">Ｑ : 要怎麼查詢我的購物金？</div>
-                            <div class="content">Ａ : 登入官網會員後，點選「會員中心」頁面，點選「我的帳戶」即會出現目前帳戶可使用的購物金額。
+                    </div>
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="headingTwo">
+                            <button type="button" class="accordion-button" data-bs-toggle="collapse"
+                                data-bs-target="#collapseTwo">
+                                Ｑ: 要怎麼查詢我的購物金？</button>
+                        </h2>
+                        <div id="collapseTwo" class="accordion-collapse collapse show" data-bs-parent="#myAccordion">
+                            <div class="card-body">
+                                <p>Ａ : 登入官網會員後，點選「會員中心」頁面，點選「我的帳戶」即會出現目前帳戶可使用的購物金額。</p>
                             </div>
                         </div>
-                        <hr>
-                        <div class="container">
-                            <div class="label">Ｑ : 沒有在領取的期內取貨怎麼辦？</div>
-                            <div class="content">Ａ : 若您逾期未至門市取貨，商品將會退回本公司，訂單亦將自動取消無法重新配送，若您仍需要該商品，請重新至官網訂購。 提醒您，若您有3次以上(含3次)未於通知期限內至該指定門巿取貨，未來您將無法再次訂購商品，同時您的帳號使用權也將被取消。請您珍惜寶貴的訂購資源及維護自身權益。</div>
+                    </div>
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="headingThree">
+                            <button type="button" class="accordion-button collapsed" data-bs-toggle="collapse"
+                                data-bs-target="#collapseThree">Ｑ : 沒有在領取的期內取貨怎麼辦？</button>
+                        </h2>
+                        <div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#myAccordion">
+                            <div class="card-body">
+                                <p>Ａ : 若您逾期未至門市取貨，商品將會退回本公司，訂單亦將自動取消無法重新配送，若您仍需要該商品，請重新至官網訂購。
+                                    提醒您，若您有3次以上(含3次)未於通知期限內至該指定門巿取貨，未來您將無法再次訂購商品，同時您的帳號使用權也將被取消。請您珍惜寶貴的訂購資源及維護自身權益。</p>
+                            </div>
                         </div>
-                        <hr>
                     </div>
                 </div>
             </div>
@@ -51,16 +66,6 @@
     </div>
 </template>
 <script>
-export default {
-    data() {
-
-    },
-    methods: {
-        onclick: {
-
-        }
-    }
-}
 </script>
 <style scoped>
 .container {
@@ -109,7 +114,7 @@ export default {
     flex-wrap: wrap;
     height: 100%;
     /* background-color: yellowgreen; */
-    margin-right: 5px;
+    margin-right: auto;
 }
 
 .wordsize li {
