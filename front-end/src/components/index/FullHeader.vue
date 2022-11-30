e80ff384b85d9ff7627ef81c0606db2b7acfb989
travislin5 <67934666+travislin5@users.noreply.github.com>
2022-11-30 23:12:48 +0800

    fixed header 範例

--- front-end/src/components/index/FullHeader.vue
+++ front-end/src/components/index/FullHeader.vue
@@ -1,5 +1,5 @@
 <template>
-    <header class="header-wrapper">
+    <header class="header-wrapper" :class="{fixed:fixed}" id="header">
         <!-- HEADER_INNER 限制內容物最寬1200px，包住search和nav -->
         <div class="contents-inner">
             <div class="search-wrap">
@@ -98,6 +98,14 @@
 <script>
 export default {
     name: 'FullHeader',
+       created() {
+      window.addEventListener("scroll", this.handleScroll);
+    },
+    data() {
+        return {
+            fixed: false,
+        }
+    },
     methods: {
         toShoppingPage(){
             this.$router.push("/home/shoppingcart");
@@ -105,6 +113,10 @@ export default {
         toMemberPage(){
              this.$router.push("/home/member");
         },
+         handleScroll() {
+            let headerHight =  document.getElementById("header").offsetHeight
+            this.fixed = window.scrollY > headerHight ? true : false;
+      },
     },
 }
 </script>
@@ -269,5 +281,13 @@ button {
 .sub-list {
     margin-left: auto;
 }
+
+ .fixed {
+    position: fixed;
+    z-index: 10;
+    top: 0;
+    width: 100%;
+    /* background-color: #fffbe6; */
+  }
 
 </style>
