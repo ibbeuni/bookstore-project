9e280ef49fbdb0ced6e15771962f4fc7e9aac5d8
ibbeuni <ibbeuniii@gmail.com>
2022-12-02 01:45:53 +0800

    重切video

--- front-end/src/components/index/VideoBanner.vue
+++ front-end/src/components/index/VideoBanner.vue
@@ -1,9 +1,18 @@
 <template>
     <section class="contents">
         <div class="video-banner-wrap">
+            <div class="swiper-container">
+                <ul class="swiper-wrapper">
+                    <li class="swiper-slide">
+                        <div class="blur-img-box">
+                            <img src="@/assets/img/index/video_banner/v-banner01.jpg" alt="">
+                        </div>
+                    </li>
+                </ul>
+            </div>
             <div class="contents-inner">
-                <div class="contents-inner">
-                    <!-- 標題 -->
+                <!-- VIDEO_CONTENTS -->
+                <div class="video-contents">
                     <div class="title-wrap">
                         <h2 class="title-heading">線上讀書會</h2>
                         <div class="right-area">
@@ -14,30 +23,37 @@
                             </a>
                         </div>
                     </div>
-                    <div class="video-swiper-container">
-                        <ul class="video-swiper-wrapper">
-                            <li class="video-swiper-slide">
+                    <!-- swiper-container -->
+                    <div class="swiper-container-fade">
+                        <ul class="swiper-wrapper-fade">
+                            <li class="swiper-slide-fade">
                                 <div class="video-banner-area">
                                     <div class="video-title-box">
-                                        <h2>我是影片的標題</h2>
-                                        <small>與談人姓名</small>
+                                        <h2 class="video-title">我是影片的標題</h2>
+                                        <small class="video-host">與談人姓名</small>
                                     </div>
                                     <div class="video-visual-box">
-                                        <div class="prod-area">
+                                        <div class="prod-area hidden">
                                             <div class="prod-thumb-box">
                                                 <a href="">
-                                                    <img src="@/assets/img/books/30101.png" alt="">
+                                                    <div class="img-box">
+                                                        <img src="@/assets/img/books/30101.png" alt="">
+                                                    </div>
                                                 </a>
-                                                <p>書籍名稱</p>
+                                            </div>
+                                            <div class="prod-info-box">
+                                                <a href="">
+                                                    <span class="prod-auther">作者名</span>
+                                                </a>
                                             </div>
                                         </div>
-                                        <div class="video-frame-wrap">
-                                            <iframe width="685px" height="403px"
-                                                src="https://www.youtube.com/embed/K2vBHUUqWoI"
-                                                title="YouTube video player" frameborder="0"
-                                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                                                allowfullscreen></iframe>
-                                        </div>
+                                        <a href="" class="video-img-link">
+                                            <div class="img-wrap">
+                                                <div class="play-btn">
+                                                    <font-awesome-icon icon="fa-solid fa-play" />
+                                                </div>
+                                            </div>
+                                        </a>
                                     </div>
                                 </div>
                             </li>
@@ -85,11 +101,75 @@ button {
     margin: 0 auto;
 }
 
+/* casting */
+.video-banner-wrap {
+    position: relative;
+}
+
+.swiper-container {
+    position: absolute;
+    width: 100%;
+    height: 470px;
+    left: 0;
+    top: 0;
+    margin: 0 auto;
+}
+
+.swiper-wrapper {
+    position: relative;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    z-index: 1;
+    box-sizing: content-box;
+}
+
+.swiper-slide {
+    display: list-item;
+
+}
+
+.blur-img-box {
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: 2;
+    overflow: hidden;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.6);
+}
+
+.blur-img-box img {
+    display: block;
+    width: 100%;
+    top: 50%;
+    left: 0;
+    transform: translateY(-50%);
+    position: absolute;
+    /* vertical-align: middle; */
+}
+
+.video-contents {
+    position: relative;
+    padding-top: 107px;
+    z-index: 2;
+}
+
 .title-wrap {
     padding-bottom: 30px;
+    position: relative;
     display: flex;
     justify-content: flex-start;
+    align-items: center;
     flex-direction: row;
+}
+
+.title-heading {
+    line-height: 40px;
+    display: inline-block;
+    position: relative;
+    color: var(--background-color);
 }
 
 .right-area {
@@ -101,72 +181,140 @@ button {
     font-size: 1rem;
 }
 
-/* casting */
-.video-banner-wrap {
-    position: relative;
-    /* background-color: red; */
+.btn-more-plus {
+    color: var(--background-color);
 }
 
-.video-swiper-container {
-    /* background-color: aqua; */
-    width: 100%;
+.swiper-container-fade {
     margin: 0 auto;
+    position: relative;
+    overflow: hidden;
+    list-style: none;
     padding: 0;
-    z-index: 99;
+    z-index: 1;
 }
 
-.video-swiper-wrapper {
-    /* background-color: red; */
+.swiper-wrapper-fade {
+    position: relative;
     width: 100%;
     height: 100%;
+    z-index: 1;
     display: flex;
     box-sizing: content-box;
+
 }
 
-.video-swiper-slide {
-    height: 100%;
-    width: 100%;
+.swiper-slide-fade {
+    width: 1200px;
+    opacity: 1;
+    position: relative;
+    display: list-item;
 }
 
 .video-banner-area {
     display: flex;
-    justify-content: center;
-    width: 100%;
-    height: 300px;
-    
+    justify-content: space-between;
+    align-items: flex-start;
+    flex-direction: row;
 }
 
 .video-title-box {
+    flex: 1;
+    color: var(--background-color);
     padding-right: 30px;
-    margin-right: auto;
+}
+
+.video-title {
+    overflow: hidden;
+    max-height: 120px;
+    line-height: 40px;
+    color: var(--background-color);
+}
+
+.video-host {
+    margin-top: 17px;
+    line-height: 23px;
+    color: var(--background-color);
 }
 
 .video-visual-box {
     display: flex;
-    margin-left: 30px;
-    position: relative;
+    justify-content: space-between;
+    align-items: flex-end;
+    flex-direction: row;
 }
 
 .prod-area {
     width: 118px;
+    margin-right: 36px;
     margin: auto auto 0;
-    /* margin-right: 36px;
-    margin-top: auto; */
-}
-
-.prod-thumb-box {
     margin-top: auto;
 }
 
-.prod-thumb-box img {
+.prod-thumb-box {
     width: 118px;
+    position: relative;
+    overflow: hidden;
+    margin-top: auto;
 }
 
-.video-frame-wrap {
-    border-radius: 18px;
+.img-box {
     overflow: hidden;
-    margin-left: 30px;
+    position: relative;
+    box-sizing: border-box;
+    cursor: pointer;
+}
+
+.prod-thumb-box img {
+    max-height: calc((118px - 2px) * 1.168 - 10px);
     width: 100%;
+    min-height: 50px;
+    vertical-align: middle;
+    border: 0;
+}
+
+.prod-info-box {
+    margin-top: 16px;
+}
+
+.prod-auther {
+    color: var(--font-color);
+}
+
+.video-img-link {
+    display: block;
+    width: 685px;
+    height: 403px;
+    border-radius: 30px;
+    overflow: hidden;
+    text-decoration: none;
+    cursor: pointer;
+}
+
+.img-wrap {
+    background-image: url('@/assets/img/index/video_banner/v-box01.jpg');
     height: 100%;
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.play-btn {
+    width: 70px;
+    height: 70px;
+    color: var(--background-color);
+    font-size: 30px;
+    border: 1px solid #fff;
+    border-radius: 50%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.hidden {
+    display: none;
 }
 </style>
