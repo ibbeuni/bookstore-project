e9ae0becb4e147261e5d4d7b52d4710d9dad5a9c
travislin5 <67934666+travislin5@users.noreply.github.com>
2022-12-07 18:04:26 +0800

    修改會員調整

--- front-end/src/Backstage/AddMember.vue
+++ front-end/src/Backstage/AddMember.vue
@@ -1,12 +1,11 @@
 <template>
   <div>
-    <p><label for="title">會員姓名:</label><input v-model="memberData.name" type="text" required /></p>
-    <p><label for="title">會員信箱:</label><input v-model="memberData.email" type="text" required /></p>
-    <p><label for="title">會員密碼:</label><input v-model="memberData.password" type="text" required /></p>
-    <p><label for="title">書會員電話:</label><input v-model="memberData.phone" type="text" required /></p>
-    <p><label for="title">會員地址:</label><input v-model="memberData.address" type="text" required /></p>
-    <p><label for="title">會員性別:</label><input v-model="memberData.gender" type="text" required /></p>
-    <p><label for="title">會員生日:</label><input v-model="memberData.birth" type="text" required /></p>
+    <p><label for="title">會員姓名:</label><input v-model="memberData.member_name" type="text" required /></p>
+    <p><label for="title">會員信箱:</label><input v-model="memberData.member_id" type="text" required /></p>
+    <p><label for="title">會員密碼:</label><input v-model="memberData.member_password" type="text" required /></p>
+    <p><label for="title">書會員電話:</label><input v-model="memberData.member_phone" type="text" required /></p>
+    <p><label for="title">會員地址:</label><input v-model="memberData.member_address" type="text" required /></p>
+    <p><label for="title">會員生日:</label><input v-model="memberData.member_birthday" type="text" required /></p>
 
     <div>
       <button class="buttomStyle" style="width: 150px; height: 50px">
@@ -25,7 +24,7 @@ export default {
     };
   },
   mounted() {
-    if(this.$route.params['name'] != undefined){
+    if(this.$route.params['member_name'] != undefined){
         console.log(this.$route.params)
         this.memberData = this.$route.params
     }else{
