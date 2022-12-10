0097e13e9198a76308fcb8bce009e68f7cf29804
travislin5 <67934666+travislin5@users.noreply.github.com>
2022-12-11 03:23:07 +0800

    關鍵字搜尋書名、粽合項目點擊渲染

--- front-end/src/router/index.js
+++ front-end/src/router/index.js
@@ -32,6 +32,13 @@ import addBook from '../Backstage/addBook.vue';
 import alterBook from '../Backstage/alterBook.vue';
 Vue.use(VueRouter);
 
+//获取原型对象上的push函数
+const originalPush = VueRouter.prototype.push
+//修改原型对象中的push方法
+VueRouter.prototype.push = function push(location) {
+  return originalPush.call(this, location).catch(err => err)
+}
+
 const routes = [
     {
         path: '*',
