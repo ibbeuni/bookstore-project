29fba07008619fe09c908fc29dc2f195adb7c49a
GavinWU1 <catmimi1212@gmail.com>
2022-12-13 16:30:49 +0800

    AQpage

--- front-end/src/views/AskedQ.vue
+++ front-end/src/views/AskedQ.vue
@@ -7,29 +7,21 @@
 
         </div>
         <div class="content">
-            <div class="card">
-                <ul class="wordsize">
-                    <li><a href="/Users/catmimi1212/Desktop/大專題/靜態畫面/accordion/zzz.html">關於我們</a></li>
-                    <li>會員常見問題</li>
-                    <li>購物常見問題</li>
-                    <li>配送取貨問題</li>
-                    <li>退換貨及退款</li>
-                    <li>產品維護方式</li>
-                    <li>隱私政策</li>
-                    <li>使用者條款</li>
-                </ul>
-            </div>
             <div class="card_1">
                 <div class="accordion" id="myAccordion">
                     <h1>會員常見問題</h1>
                     <div class="accordion-item">
                         <div class="accordion-header" id="headingOne">
                             <button type="button" class="accordion-button collapsed" data-bs-toggle="collapse"
-                                data-bs-target="#collapseOne">Ｑ : 忘記帳號密碼怎麼辦？</button>
+                                data-bs-target="#collapseOne">Ｑ : 沒有在領取的期內取貨怎麼辦？</button>
                         </div>
                         <div id="collapseOne" class="accordion-collapse collapse" data-bs-parent="#myAccordion">
                             <div class="card-body">
-                                <p>Ａ : 可以直接來信或來電與客服人員聯繫協助查詢，忘記密碼則可以點選忘記密碼，我們會發email到您的信箱內，買家可以自行收信更改密碼唷!</p>
+                                <p>Ａ :
+                                    <br>
+                                    若您逾期未至門市取貨，商品將會退回本公司，訂單亦將自動取消無法重新配送，若您仍需要該商品，請重新至官網訂購。
+                                    提醒您，若您有3次以上(含3次)未於通知期限內至該指定門巿取貨，未來您將無法再次訂購商品，同時您的帳號使用權也將被取消。請您珍惜寶貴的訂購資源及維護自身權益。
+                                </p>
                             </div>
                         </div>
                     </div>
@@ -37,23 +29,74 @@
                         <h2 class="accordion-header" id="headingTwo">
                             <button type="button" class="accordion-button" data-bs-toggle="collapse"
                                 data-bs-target="#collapseTwo">
-                                Ｑ: 要怎麼查詢我的購物金？</button>
+                                Ｑ: 國內及離島可選擇的配送方式及運費計算？</button>
                         </h2>
                         <div id="collapseTwo" class="accordion-collapse collapse show" data-bs-parent="#myAccordion">
                             <div class="card-body">
-                                <p>Ａ : 登入官網會員後，點選「會員中心」頁面，點選「我的帳戶」即會出現目前帳戶可使用的購物金額。</p>
+                                <p>Ａ :
+                                    <br>
+                                    ※海外買家不適用
+                                    <br>
+                                    1. 7-11取貨付款 80元
+                                    <br>
+                                    2. 宅配通配送到府 80元 (離島地區無提供宅配服務)
+                                    <br>
+                                    ※ 離島地區因無提供宅配服務，買家請務必選擇7-11取貨付款方式唷！若付款寄送方式選擇錯誤，賣場將會直接取消您的訂單並且將已付款項退回給您！
+                                    <br>
+                                    ※
+                                    離島買家特別注意，因超商取件金額限制50元-3000元內，若您的訂單金額超過3000元，會無法選取超商取件付款服務，再請您重新分開下兩張訂單，即可選擇7-11取件貨到付款。
+                                    <br>
+                                    ※ 商品到齊出貨後約2~3天可送達指定門市(包裹異常外)
+                                    並且發送簡訊通知(請留意會員手機號碼正確，將發送至您訂單成立時所填寫之手機號碼，號碼不需填入任何符號及空白鍵，以免傳訊失敗。)
+                                    或可自行依照寄出日期推算2-3個工作天便可至門市取貨。；若使用宅配方式配送，包裹寄出約2個工作天便會抵達指定地點，請買家留意電話通知。
+                                </p>
                             </div>
                         </div>
                     </div>
                     <div class="accordion-item">
                         <h2 class="accordion-header" id="headingThree">
                             <button type="button" class="accordion-button collapsed" data-bs-toggle="collapse"
-                                data-bs-target="#collapseThree">Ｑ : 沒有在領取的期內取貨怎麼辦？</button>
+                                data-bs-target="#collapseThree">Ｑ : 超過領取期限，包裹被退回至賣場，怎麼辦呢？</button>
                         </h2>
                         <div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#myAccordion">
                             <div class="card-body">
-                                <p>Ａ : 若您逾期未至門市取貨，商品將會退回本公司，訂單亦將自動取消無法重新配送，若您仍需要該商品，請重新至官網訂購。
-                                    提醒您，若您有3次以上(含3次)未於通知期限內至該指定門巿取貨，未來您將無法再次訂購商品，同時您的帳號使用權也將被取消。請您珍惜寶貴的訂購資源及維護自身權益。</p>
+                                <p>Ａ :
+                                    <br>
+                                    若還需要購買，請買家重新下單，商品會依照當時優惠活動為主，賣場無提供重寄包裹之服務。提醒您，若您有3次以上(含3次)未於通知期限內至該指定門巿取貨，未來您將無法再次使用超商取件/貨到付款，需使用宅配通配送/線上刷卡或ATM結帳，若再次發生商品拒收造成退回問題，您的帳號使用權也將被取消。請您珍惜寶貴的訂購資源及維護自身權益。
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="headingFour">
+                            <button type="button" class="accordion-button collapsed" data-bs-toggle="collapse"
+                                data-bs-target="#collapseFour">Ｑ : 如何辦理退換貨？</button>
+                        </h2>
+                        <div id="collapseFour" class="accordion-collapse collapse" data-bs-parent="#myAccordion">
+                            <div class="card-body">
+                                <p>Ａ :
+                                    <br>
+                                    ※一律使用【7-11退貨便】
+                                    <br>
+                                    【7-11退貨便】
+                                    <br>
+                                    請於收到包裹後隔日算起7天鑑賞期內(包含例假日)，直接來信或來電與客服人員聯繫協助辦理退貨服務，信件內請留下會員帳號/訂單資訊/手機號碼/欲退貨之商品件數以及商品編號/退貨原因，以便客服人員作確認。
+                                    <br>
+                                    客服人員會將"退貨便代碼"以簡訊方式發送至您的手機內，您會在10-15分鐘內收到一組「退貨便代碼」，請帶著這組代碼連同商品三日內至全省7-11辦理即可，（產生代碼隔日開始計算），超過期限恕不提供第二組退貨便代碼。
+                                </p>
+                            </div>
+                        </div>
+                        <div class="accordion-item">
+                            <div class="accordion-header" id="headingFive">
+                                <button type="button" class="accordion-button collapsed" data-bs-toggle="collapse"
+                                    data-bs-target="#collapseFive">Ｑ : 收到的商品有瑕疵怎麼處理？</button>
+                            </div>
+                            <div id="collapseFive" class="accordion-collapse collapse" data-bs-parent="#myAccordion">
+                                <div class="card-body">
+                                    <p>Ａ :
+                                        若您收到的商品有瑕疵，或非您所訂購的商品，請趕緊於收到包裹7日內將該商品瑕疵/寄錯的細節照片Email至客服傳送給我們，並在信件內留下會員帳號/訂單資訊/手機號碼，以便客服人員確認。我們將在收到商品照片後立即為您處理。若兩日未回覆可能有漏信的情形，可請您再次來信或來電聯繫客服。
+                                    </p>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -84,9 +127,15 @@
 
 .content {
     width: 100%;
-    height: 600px;
+    height: 100%;
     /* background-color: gray; */
     display: flex;
+    /* 水平置中 */
+    justify-content: center;
+    /* 垂直置中 */
+    align-content: center;
+    flex-wrap: wrap;
+
 }
 
 .footer {
@@ -99,7 +148,6 @@
     display: flex;
     justify-content: center;
     align-items: center;
-
 }
 
 .list {
